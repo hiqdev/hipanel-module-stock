4f03bb1cf63153f35e2119c8e3141e09ed714d9e
Andrey Klochok <andreyklochok@gmail.com>
2015-09-18 13:49:21 +0300

    Fix _ram view

--- src/views/model/_ram.php
+++ src/views/model/_ram.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 <div class="form-group">
     <label class="control-label"><?= Html::activeLabel($model, 'RAM_VOLUME')?></label>
     <div class="input-group">
-        <?= Html::activeInput($model, "[$i][props]RAM_VOLUME", ['class' => 'form-control']); ?>
+        <?= Html::activeTextInput($model, "[$i][props]RAM_VOLUME", ['class' => 'form-control']); ?>
         <span class="input-group-addon">GB</span>
     </div>
 </div>

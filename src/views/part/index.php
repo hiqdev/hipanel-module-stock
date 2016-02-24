539d7e6d6b76a52d8d21b801516252a63941c84e
Andrii Vasyliev <sol@hiqdev.com>
2016-02-24 08:56:27 +0000

    minor:retabed

--- src/views/part/index.php
+++ src/views/part/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use hipanel\helpers\Url;
 use hipanel\modules\stock\grid\PartGridView;
 use hipanel\widgets\ActionBox;
@@ -11,55 +12,53 @@ $this->breadcrumbs->setItems([
 ]); ?>
 
 <?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-<?php $box = ActionBox::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
-<?php $box->beginActions() ?>
+    <?php $box = ActionBox::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
+        <?php $box->beginActions() ?>
+            <?= $box->renderCreateButton(Yii::t('app', 'Create part')) ?>
+            <?= $box->renderSearchButton() ?>
+            <?= $box->renderSorter([
+                'attributes' => [
+                    'id',
+                    'type',
+                    'brand',
+                    'partno',
+                    'model',
+                    'serial',
+                    'time',
+                ],
+            ]) ?>
+            <?= $box->renderPerPage() ?>
+        <?php $box->endActions() ?>
+        <?php $box->renderBulkActions([
+            'items' => [
+                $box->renderBulkButton(Yii::t('app', 'Update'), Url::to('@part/update')),
+                $box->renderBulkButton(Yii::t('app', 'Move'), Url::to('@part/bulk-move')),
+                $box->renderBulkButton(Yii::t('app', 'To move by one'), Url::to('@part/move')),
+                $box->renderBulkButton(Yii::t('app', 'Reserve'), Url::to('@part/reserve')),
+                $box->renderBulkButton(Yii::t('app', 'Unreserve'), Url::to('@part/unreserve')),
+        //        $box->renderBulkButton(Yii::t('app', 'RMA'), Url::to('@part/rma')),
+            ],
+        ]) ?>
+        <?= $box->renderSearchForm(compact(['types', 'locations', 'brands'])) ?>
+    <?php $box->end() ?>
+    <?php $box->beginBulkForm() ?>
+        <?= PartGridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $model,
+            'columns' => [
+                'checkbox',
+                'main',
+                'partno',
+                'serial',
+                'last_move',
+                'move_type_label',
 
-<?= $box->renderCreateButton(Yii::t('app', 'Create part')) ?>
+                'move_date',
+                'order_data',
+                'DC_ticket_ID',
 
-<?= $box->renderSearchButton() ?>
-<?= $box->renderSorter([
-    'attributes' => [
-        'id',
-        'type',
-        'brand',
-        'partno',
-        'model',
-        'serial',
-        'time',
-    ],
-]) ?>
-<?= $box->renderPerPage() ?>
-<?php $box->endActions() ?>
-<?php $box->renderBulkActions([
-    'items' => [
-        $box->renderBulkButton(Yii::t('app', 'Update'), Url::to('@part/update')),
-        $box->renderBulkButton(Yii::t('app', 'Move'), Url::to('@part/bulk-move')),
-        $box->renderBulkButton(Yii::t('app', 'To move by one'), Url::to('@part/move')),
-        $box->renderBulkButton(Yii::t('app', 'Reserve'), Url::to('@part/reserve')),
-        $box->renderBulkButton(Yii::t('app', 'Unreserve'), Url::to('@part/unreserve')),
-//        $box->renderBulkButton(Yii::t('app', 'RMA'), Url::to('@part/rma')),
-    ],
-]) ?>
-<?= $box->renderSearchForm(compact(['types', 'locations', 'brands'])) ?>
-<?php $box->end() ?>
-<?php $box->beginBulkForm() ?>
-<?= PartGridView::widget([
-    'dataProvider' => $dataProvider,
-    'filterModel' => $model,
-    'columns' => [
-        'checkbox',
-        'main',
-        'partno',
-        'serial',
-        'last_move',
-        'move_type_label',
-
-        'move_date',
-        'order_data',
-        'DC_ticket_ID',
-
-        'actions',
-    ],
-]) ?>
-<?php $box->endBulkForm() ?>
+                'actions',
+            ],
+        ]) ?>
+    <?php $box->endBulkForm() ?>
 <?php Pjax::end() ?>

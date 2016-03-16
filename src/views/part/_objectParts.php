13ba192fd70544fcb9475ecc18a042344c1658e5
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-03-16 13:59:41 +0200

    Object parts view redone with GridView

--- src/views/part/_objectParts.php
+++ src/views/part/_objectParts.php
@@ -7,24 +7,58 @@
 
 use hipanel\base\Lang;
 use yii\helpers\Html;
-// TODO: design
-foreach ($data as $type => $models) {
-    echo Html::beginTag('p');
-    echo Html::tag('strong' , Yii::t('hipanel/stock/model-types', Lang::lang($type)) . ': ');
-    $models_partno = [];
-    $serials = [];
-    foreach ($models as $model_id => $parts) {
-        $modelLink = Html::a(reset($parts)->partno, ['@model/view', 'id' => $model_id]);
-        $models_partno[] = (count($parts) > 1 ? count($parts) . 'x' : '') . $modelLink;
 
-        foreach ($parts as $part) {
-            $serials[] = Html::a($part->serial, ['@part/view', 'id' => $part->id]);
-        }
-    }
+echo \hipanel\grid\GridView::widget([
+    'dataProvider' => new \yii\data\ArrayDataProvider([
+        'pagination' => false,
+        'allModels' => $data,
+        'sort' => false,
+    ]),
+    'summary' => false,
+    'columns' => [
+        [
+            'attribute' => 'type',
+            'format' => 'html',
+            'label' => Yii::t('hipanel/stock', 'Type'),
+            'options' => [
+                'style' => 'width: 20%',
+            ],
+            'value' => function ($models, $key) {
+                return Html::tag('strong', Yii::t('hipanel/stock', Lang::lang($key)));
+            }
+        ],
+        [
+            'label' => Yii::t('hipanel/stock', 'Model'),
+            'attribute' => 'model',
+            'options' => [
+                'style' => 'width: 40%',
+            ],
+            'format' => 'raw',
+            'value' => function ($models) {
+                $models_partno = [];
+                foreach ($models as $model_id => $parts) {
+                    $modelLink = Html::a(reset($parts)->partno, ['@model/view', 'id' => $model_id]);
+                    $models_partno[] = (count($parts) > 1 ? count($parts) . 'x' : '') . $modelLink;
+                }
 
-    echo implode(', ', $models_partno);
-    echo ' - ';
-    echo implode(', ', $serials);
+                return implode(', ', $models_partno);
+            }
+        ],
+        [
+            'attribute' => 'serials',
+            'format' => 'html',
+            'label' => Yii::t('hipanel/stock', 'Serials'),
+            'value' => function ($models) {
+                $serials = [];
 
-    echo Html::endTag('p');
-}
+                foreach ($models as $model_id => $parts) {
+                    foreach ($parts as $part) {
+                        $serials[] = Html::a($part->serial, ['@part/view', 'id' => $part->id]);
+                    }
+                }
+
+                return implode(', ', $serials);
+            }
+        ]
+    ]
+]);

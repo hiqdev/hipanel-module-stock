2448ccec2fa0ef0edf80f1229d54b1b3aca00b75
tofid <andreyklochok@gmail.com>
2016-11-03 13:17:20 +0200

    Added i18n config, chagned hisite config translation to `:`

---
+++ src/config/i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'languages' => ['ru'],
+    'sourcePath' => dirname(__DIR__),
+    'messagePath' => dirname(__DIR__) . '/messages',
+    'sort' => true,
+    'removeUnused' => true,
+    'ignoreCategories' => [
+        'hipanel',
+    ]
+];

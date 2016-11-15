968de0a99a12618aff42ef773a75958bc0e8c09b
tofid <andreyklochok@gmail.com>
2016-11-15 16:04:44 +0200

    Recomposed the fields

--- src/views/model/_server.php
+++ src/views/model/_server.php
@@ -3,21 +3,21 @@ use yii\helpers\Html;
 
 ?>
 <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-6">
         <div class="form-group">
             <label><?= Html::activeLabel($model, 'units_qty')?></label>
             <?= Html::activeInput('number', $model, "[$i][props]units_qty", ['class' => 'form-control', 'min' => '1', 'max' => '100']); ?>
         </div>
     </div>
     <!-- /.col-md-3 -->
-    <div class="col-md-3">
+    <div class="col-md-6">
         <div class="form-group">
             <label><?= Html::activeLabel($model, 'ram_qty')?></label>
             <?= Html::activeInput('number', $model, "[$i][props]ram_qty", ['class' => 'form-control', 'min' => '1', 'max' => '100']); ?>
         </div>
     </div>
     <!-- /.col-md-3 -->
-    <div class="col-md-6">
+    <div class="col-md-12">
         <div class="form-group">
             <label><?= Html::activeLabel($model, 'cpu_qty')?></label>
             <?= Html::activeTextInput($model, "[$i][props]cpu_qty", ['class' => 'form-control']); ?>

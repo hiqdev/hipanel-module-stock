ecf5dec7d9226c39d16230672cc8fa3dab9aba93
Andrii Vasyliev <sol@hiqdev.com>
2018-05-07 16:04:41 +0000

    csfixed

--- src/views/part/_search.php
+++ src/views/part/_search.php
@@ -20,22 +20,29 @@ use yii\helpers\Html;
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('model_types')->widget(RefCombo::class, ['gtype' => 'type,model',
-        'multiple' => true,]) ?>
+    <?= $search->field('model_types')->widget(RefCombo::class, [
+        'gtype' => 'type,model',
+        'multiple' => true,
+    ]) ?>
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('model_brands')->widget(RefCombo::class, ['gtype' => 'type,brand',
-        'multiple' => true,]) ?>
+    <?= $search->field('model_brands')->widget(RefCombo::class, [
+        'gtype' => 'type,brand',
+        'multiple' => true,
+    ]) ?>
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
     <div class="form-group">
         <?= Html::tag('label', Yii::t('hipanel:stock', 'Last move date'), ['class' => 'control-label']); ?>
-        <?= DatePicker::widget(['model' => $search->model,
+        <?= DatePicker::widget([
+            'model' => $search->model,
             'attribute' => 'move_time',
-            'pluginOptions' => ['autoclose' => true,
-                'format' => 'yyyy-mm-dd',],
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd',
+            ],
         ]) ?>
     </div>
 </div>
@@ -43,12 +50,15 @@ use yii\helpers\Html;
 <div class="col-md-4 col-sm-6 col-xs-12">
     <div class="form-group">
         <?= Html::tag('label', Yii::t('hipanel:stock', 'Created range'), ['class' => 'control-label']) ?>
-        <?= DatePicker::widget(['model' => $search->model,
+        <?= DatePicker::widget([
+            'model' => $search->model,
             'type' => DatePicker::TYPE_RANGE,
             'attribute' => 'create_time_from',
             'attribute2' => 'create_time_till',
-            'pluginOptions' => ['autoclose' => true,
-                'format' => 'yyyy-mm-dd',],
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd',
+            ],
         ]) ?>
     </div>
 </div>
@@ -62,15 +72,19 @@ use yii\helpers\Html;
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('place_in')->widget(StaticCombo::class, ['data' => $locations,
+    <?= $search->field('place_in')->widget(StaticCombo::class, [
+        'data' => $locations,
         'hasId' => true,
-        'multiple' => true,]) ?>
+        'multiple' => true,
+    ]) ?>
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('currency')->widget(StaticCombo::class, ['data' => ['usd' => 'USD', 'eur' => 'EUR'],
+    <?= $search->field('currency')->widget(StaticCombo::class, [
+        'data' => ['usd' => 'USD', 'eur' => 'EUR'],
         'hasId' => true,
-        'multiple' => false,]) ?>
+        'multiple' => false,
+    ]) ?>
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('serial_like') ?></div>
@@ -78,7 +92,9 @@ use yii\helpers\Html;
 <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('order_no_ilike') ?></div>
 
 <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('limit') ?></div>
-<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('company_id')->dropDownList($search->model->companies, ['prompt' => Yii::t('hipanel:stock', 'Company')]) ?></div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('company_id')->dropDownList($search->model->companies, ['prompt' => Yii::t('hipanel:stock', 'Company')]) ?>
+</div>
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('id_in')->widget(PartCombo::class, ['multiple' => true]) ?>
 </div>

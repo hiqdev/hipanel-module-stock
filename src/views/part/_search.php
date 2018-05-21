e62ea691cd04a15fdd3f66cc3d9bc8dedfff02e0
Andrey Klochok <andreyklochok@gmail.com>
2018-05-21 14:15:05 +0300

    Added DateRangePicker to Part search form, fixed html markup

--- src/views/part/_search.php
+++ src/views/part/_search.php
@@ -7,6 +7,7 @@ use hipanel\modules\stock\widgets\combo\SourceCombo;
 use hiqdev\combo\StaticCombo;
 use hipanel\widgets\RefCombo;
 use hipanel\widgets\DatePicker;
+use hiqdev\yii2\daterangepicker\DateRangePicker;
 use yii\helpers\Html;
 
 /**
@@ -15,111 +16,100 @@ use yii\helpers\Html;
 ?>
 
 
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('partno_like')->widget(PartnoCombo::class) ?>
-    </div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('partno_like')->widget(PartnoCombo::class) ?>
+</div>
 
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('model_types')->widget(RefCombo::class, [
-            'gtype' => 'type,model',
-            'multiple' => true,
-        ]) ?>
-    </div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('model_types')->widget(RefCombo::class, [
+        'gtype' => 'type,model',
+        'multiple' => true,
+    ]) ?>
+</div>
 
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('state')->widget(RefCombo::class, [
-            'gtype' => 'state,part',
-            'multiple' => false,
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('state')->widget(RefCombo::class, [
+        'gtype' => 'state,part',
+        'multiple' => false,
+    ]) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('model_brands')->widget(RefCombo::class, [
+        'gtype' => 'type,brand',
+        'multiple' => true,
+    ]) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('serial_like') ?></div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('id_in')->widget(PartCombo::class, ['multiple' => true]) ?>
+</div>
+
+
+<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('move_descr_like') ?></div>
+
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('src_name_like')->widget(SourceCombo::class) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('dst_name_like')->widget(DestinationCombo::class) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('order_no_ilike') ?></div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('company_id')->dropDownList($search->model->companies, ['prompt' => Yii::t('hipanel:stock', 'Company')]) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('place_in')->widget(StaticCombo::class, [
+        'data' => $locations,
+        'hasId' => true,
+        'multiple' => true,
+    ]) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('currency')->widget(StaticCombo::class, [
+        'data' => ['usd' => 'USD', 'eur' => 'EUR'],
+        'hasId' => true,
+        'multiple' => false,
+    ]) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('limit') ?></div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="form-group">
+        <?= Html::tag('label', Yii::t('hipanel:stock', 'Last move date'), ['class' => 'control-label']); ?>
+        <?= DatePicker::widget([
+            'id' => 'move_time_date-picker',
+            'model' => $search->model,
+            'attribute' => 'move_time',
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd',
+            ],
         ]) ?>
     </div>
 </div>
 
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('model_brands')->widget(RefCombo::class, [
-            'gtype' => 'type,brand',
-            'multiple' => true,
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="form-group">
+        <?= Html::tag('label', Yii::t('hipanel:stock', 'Created range'), ['class' => 'control-label']) ?>
+        <?= DateRangePicker::widget([
+            'id' => 'create_time-date-range-picker',
+            'model' => $search->model,
+            'attribute' => 'create_time_from',
+            'attribute2' => 'create_time_till',
+            'options' => [
+                'class' => 'form-control',
+            ],
+            'dateFormat' => 'yyyy-MM-dd',
         ]) ?>
-    </div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('serial_like') ?></div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('id_in')->widget(PartCombo::class, ['multiple' => true]) ?>
-    </div>
-</div>
-
-
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('move_descr_like') ?></div>
-
-
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('src_name_like')->widget(SourceCombo::class) ?>
-    </div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('dst_name_like')->widget(DestinationCombo::class) ?>
-    </div>
-</div>
-
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('order_no_ilike') ?></div>
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('company_id')->dropDownList($search->model->companies, ['prompt' => Yii::t('hipanel:stock', 'Company')]) ?>
-    </div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('place_in')->widget(StaticCombo::class, [
-            'data' => $locations,
-            'hasId' => true,
-            'multiple' => true,
-        ]) ?>
-    </div>
-</div>
-
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('currency')->widget(StaticCombo::class, [
-            'data' => ['usd' => 'USD', 'eur' => 'EUR'],
-            'hasId' => true,
-            'multiple' => false,
-        ]) ?>
-    </div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('limit') ?></div>
-</div>
-
-<div class="col-md-12">
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <div class="form-group">
-            <?= Html::tag('label', Yii::t('hipanel:stock', 'Last move date'), ['class' => 'control-label']); ?>
-            <?= DatePicker::widget([
-                'model' => $search->model,
-                'attribute' => 'move_time',
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format' => 'yyyy-mm-dd',
-                ],
-            ]) ?>
-        </div>
-    </div>
-
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <div class="form-group">
-            <?= Html::tag('label', Yii::t('hipanel:stock', 'Created range'), ['class' => 'control-label']) ?>
-            <?= DatePicker::widget([
-                'model' => $search->model,
-                'type' => DatePicker::TYPE_RANGE,
-                'attribute' => 'create_time_from',
-                'attribute2' => 'create_time_till',
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format' => 'yyyy-mm-dd',
-                ],
-            ]) ?>
-        </div>
     </div>
 </div>
 

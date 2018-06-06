872ee89dd40a9e2d904d00941d17e2448c074808
Dmytro Naumenko <d.naumenko.a@gmail.com>
2018-06-06 15:25:29 +0300

    Update StockSidebarMenuCest.php

--- tests/acceptance/admin/StockSidebarMenuCest.php
+++ tests/acceptance/admin/StockSidebarMenuCest.php
@@ -9,7 +9,7 @@ class StockSidebarMenuCest
 {
     public function ensureMenuIsOk(Admin $I)
     {
-        (new SidebarMenu($I))->ensureContains('Stock',[
+        (new SidebarMenu($I))->ensureContains('Stock', [
             'Models' => '@model/index',
             'Parts' => '@part/index',
             'History' => '@move/index',

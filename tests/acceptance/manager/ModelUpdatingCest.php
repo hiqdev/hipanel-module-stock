9cdeaeac76956f4886e11d57c554263485e6d9bc
Artem Pakhomov <setnemo@gmail.com>
2018-10-31 14:38:58 +0200

    Added new tests for check 'update' functionality in Model and Parts (#34)
    * need improve upd part scenario
    
    * added PartsUpdatingCest.php
    
    * Added ModelUpdatingCest.php
    
    * Deleted login(), because Create extends Authenticated

---
+++ tests/acceptance/manager/ModelUpdatingCest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace hipanel\modules\stock\tests\acceptance\manager;
+
+use hipanel\helpers\Url;
+use hipanel\modules\stock\tests\_support\Page\model\Create;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+use hipanel\tests\_support\Step\Acceptance\Manager;
+
+class ModelUpdatingCest
+{
+    /**
+     * Create new model, update PartNo and check result
+     *
+     * @param Manager $I
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function ensureICanCreateSeveralModel(Manager $I):void
+    {
+        $page = new Create($I);
+        $newValue = 'UPD_TEST';
+
+        $I->needPage(Url::to('@model/create'));
+        $modelData = $this->getModelData('RAM', 'Kingston', '32GB DDR3');
+        $page->fillModelFields($modelData);
+        $I->pressButton('Save');
+        $urlDetails = $page->seeModelWasCreated();
+        $I->click("//a[contains(text(), 'Update')]");
+
+        $newValue .= $modelData['uid'];
+        (new Input($I, "//input[@value='".$modelData['partno']."']"))
+            ->setValue($newValue);
+        $I->pressButton('Save');
+        $I->waitForPageUpdate();
+        $I->seeInCurrentUrl('stock/model/view?id='.$urlDetails);
+        $I->see($newValue, '//td/a');
+    }
+
+    /**
+     * @param $type
+     * @param $brand
+     * @param $groupId
+     * @return array
+     */
+    protected function getModelData($type, $brand, $groupId): array
+    {
+        $uid = uniqid();
+
+        return [
+            'uid'       => $uid,
+            'type'      => $type,
+            'brand'     => $brand,
+            'group_id'  => $groupId,
+            'model'     => 'MG_TEST_MODEL' . $uid,
+            'partno'    => 'UP_TEST_' . $uid,
+            'url'       => 'test_url',
+            'short'     => 'Short description',
+            'descr'     => 'Extended description'
+        ];
+    }
+}
---
+++ tests/acceptance/manager/PartsUpdatingCest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace hipanel\modules\stock\tests\acceptance\manager;
+
+use hipanel\helpers\Url;
+use hipanel\modules\stock\tests\_support\Page\part\Create;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+use hipanel\tests\_support\Step\Acceptance\Manager;
+
+class PartsUpdatingCest
+{
+
+    /**
+     * Create new part, update price and check result
+     *
+     * @param Manager $I
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function ensureICanCreateAndUpdatePart(Manager $I): void
+    {
+        $page = new Create($I);
+
+        $I->needPage(Url::to('@part/create'));
+        $partData = $this->getPartData();
+        $page->fillPartFields($partData);
+        $I->pressButton('Save');
+        $urlDetails= $page->seePartWasCreated();
+
+        $price = '142.42';
+        $I->click("//a[contains(text(), 'Update')]");
+        (new Input($I, '//input[@value=42]'))
+            ->setValue($price);
+        $I->pressButton('Save');
+        $I->waitForPageUpdate();
+        $I->seeInCurrentUrl('stock/part/view?id='.$urlDetails);
+        $I->see('$'.$price, '//tbody//tr/td/span');
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPartData(): array
+    {
+        return [
+            'partno'        => 'MG_TEST_PARTNO',
+            'src_id'        => 'TEST01',
+            'dst_id'        => 'vCDN-soltest',
+            'serials'       => 'MG_TEST_PART' . uniqid(),
+            'move_descr'    => 'MG_TEST_MOVE',
+            'type'          => 'FROM OLD STOCK',
+            'price'         => 42,
+            'currency'      => 'usd',
+            'company_id'    => 'Other'
+        ];
+    }
+}

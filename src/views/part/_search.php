cadfa8de3f82e39388bda808bc7efada0972d533
Andrii Vasyliev <sol@hiqdev.com>
2020-12-08 16:22:54 +0000

    Add `reserver` filter

--- src/views/part/_search.php
+++ src/views/part/_search.php
@@ -123,6 +123,8 @@ use yii\helpers\Html;
     </div>
 </div>
 
+<div class="col-md-4 col-sm-6 col-xs-12"><?= $search->field('reserve_ilike') ?></div>
+
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('buyer_in')->widget(ClientCombo::class, [
         'multiple' => true,

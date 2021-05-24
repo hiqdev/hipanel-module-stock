4c85e026e5ee6553a9fa2b31bcf7c967ec70e8cd
Mark <83531949+Drakorgaur@users.noreply.github.com>
2021-05-24 13:41:35 +0300

    Update tests/_support/Page/model/Create.php
    Co-authored-by: Maksym Storchak <smy980807@ukr.net>

--- tests/_support/Page/model/Create.php
+++ tests/_support/Page/model/Create.php
@@ -167,7 +167,7 @@ JS
     public function updateModelWithNewPartNoData($newModelData) 
     {
         $I = $this->tester;
-        $I->click("//a[contains(text(), 'Update')]");
+        $I->click('Update');
         (new Input($I, "//input[@value='{$newModelData['partno']}']"))
             ->setValue($newModelData['newpartnpo']);
         $I->pressButton('Save');

5cc88b4ab4821f7dcfe597d9a8c1b9cf75c6278e
gadezist <andrybitko@gmail.com>
2022-08-18 12:08:15 +0300

    HP-934 moved 'Set real serials' test to PW

--- tests/playwright/e2e/manager/set-real-serials.spec.ts
+++ tests/playwright/e2e/manager/set-real-serials.spec.ts
@@ -1,10 +1,25 @@
 import { test } from "@hipanel-core/fixtures";
 import { expect } from "@playwright/test";
-
-// TODO: need to implement
+import PartsPage from "@hipanel-module-stock/e2e/pages/PartsPage";
+import Index from "@hipanel-core/page/Index";
 
 test("Ensure I can change serial to real @hipanel-module-stock @manager", async ({ managerPage }) => {
-  await managerPage.goto("/stock/part/index?PartSearch[partno]=SC815TQ-600WB&PartSearch[dst_name_in]=TEST-DS-02");
-  await expect(managerPage).toHaveTitle("Parts");
+
+  const indexPage = new Index(managerPage);
+  const partsPage = new PartsPage(managerPage);
+
+  await partsPage.gotoParts({partno: 'SC815TQ-600WB', dst_name_in: 'TEST-DS-02'});
+
+  await indexPage.hasTitle('Parts');
+  await indexPage.hasRowsOnTable(1);
+
+  await indexPage.chooseNumberRowOnTable(1);
+  await indexPage.clickDropdownBulkButton('Bulk actions', 'Set serial');
+  await partsPage.setSerialAsPartId();
+
+  await indexPage.chooseNumberRowOnTable(1)
+  await indexPage.clickDropdownBulkButton('Bulk actions', 'Set real serials');
+  await partsPage.tryToSetTwoRealSerialsForOnePart('test_real_serials_1');
+  await partsPage.setRealSerial('test_real_serials_1');
 
 });

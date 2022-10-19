b73b360e1ee6654a5733f800c3cd26b0e3123c38
gadezist <39760138+gadezist@users.noreply.github.com>
2022-10-19 17:40:51 +0300

    HP-1037 Remove PJAX form HiPanel (#133)

--- src/views/order/index.php
+++ src/views/order/index.php
@@ -1,18 +1,23 @@
 <?php
+
+use hipanel\models\IndexPageUiOptions;
+use hipanel\modules\stock\grid\OrderGridView;
+use hipanel\modules\stock\grid\OrderRepresentations;
+use hipanel\modules\stock\models\OrderSearch;
+use hipanel\widgets\IndexPage;
+use yii\data\ActiveDataProvider;
+use yii\helpers\Html;
+use yii\web\View;
+
 /**
- * @var \yii\web\View $this
- * @var \hipanel\modules\stock\models\OrderSearch $model
- * @var \hipanel\modules\stock\grid\OrderGridView $gridView
- * @var \hipanel\models\IndexPageUiOptions $uiModel
- * @var \hipanel\modules\stock\grid\OrderRepresentations $representationCollection
- * @var \yii\data\ActiveDataProvider $dataProvider
+ * @var View $this
+ * @var OrderSearch $model
+ * @var OrderGridView $gridView
+ * @var IndexPageUiOptions $uiModel
+ * @var OrderRepresentations $representationCollection
+ * @var ActiveDataProvider $dataProvider
  * @var array $types
  */
-
-use hipanel\modules\stock\grid\OrderGridView;
-use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
-use yii\helpers\Html;
 
 $this->title = Yii::t('hipanel.stock.order', 'Orders');
 $this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
@@ -23,39 +28,36 @@ $showFooter = ($uiModel->representation === 'profit-report')
 
 ?>
 
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-        <?= $page->setSearchFormData(compact('uiModel')) ?>
-        <?php $page->beginContent('main-actions') ?>
-            <?php  if (Yii::$app->user->can('order.create')) : ?>
-                <?= Html::a(Yii::t('hipanel.stock.order', 'Create order'), ['@order/create'], ['class' => 'btn btn-sm btn-success']) ?>
-            <?php endif; ?>
-        <?php $page->endContent() ?>
+    <?php $page->setSearchFormData(['uiModel' => $uiModel]) ?>
+    <?php $page->beginContent('main-actions') ?>
+        <?php  if (Yii::$app->user->can('order.create')) : ?>
+            <?= Html::a(Yii::t('hipanel.stock.order', 'Create order'), ['@order/create'], ['class' => 'btn btn-sm btn-success']) ?>
+        <?php endif; ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('sorter-actions') ?>
-            <?= $page->renderSorter([
-                'attributes' => [
-                    'time'
-                ],
-            ]) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('sorter-actions') ?>
+        <?= $page->renderSorter([
+            'attributes' => [
+                'time'
+            ],
+        ]) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('representation-actions') ?>
-            <?= $page->renderRepresentations($representationCollection) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('representation-actions') ?>
+        <?= $page->renderRepresentations($representationCollection) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('table') ?>
-            <?php $page->beginBulkForm() ?>
-            <?= OrderGridView::widget([
-                'dataProvider' => $dataProvider,
-                'filterModel' => $model,
-                'boxed' => false,
-                'showFooter' => $showFooter,
-                'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
-            ]) ?>
-            <?php $page->endBulkForm() ?>
-        <?php $page->endContent() ?>
-    <?php $page->end() ?>
-<?php Pjax::end() ?>
-
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+        <?= OrderGridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $model,
+            'boxed' => false,
+            'showFooter' => $showFooter,
+            'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
+        ]) ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
+<?php $page->end() ?>

68c8dc2ecbe73922a44c6b377a11caa432416a93
Andrii Klochok <andreyklochok@gmail.com>
2023-05-16 10:08:58 +0300

    HP-1356: show link to to the part for which the replacement was made

--- src/views/part/replace.php
+++ src/views/part/replace.php
@@ -82,7 +82,6 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 <?php DynamicFormWidget::end() ?>
-<?php Box::begin(['options' => ['class' => 'box-solid']]) ?>
 <div class="row">
     <div class="col-md-12 no">
         <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
@@ -90,5 +89,4 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
     </div>
 </div>
-<?php Box::end() ?>
 <?php ActiveForm::end() ?>

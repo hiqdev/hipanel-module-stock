17326cbb6c83ba3994dcde9c8a8f1aa98335057f
gadezist <39760138+gadezist@users.noreply.github.com>
2023-06-30 11:53:12 +0300

    HP-1439 Fix errors in Sentry on hipanel.advancedhosting.com (#147)

--- src/views/part/changeModel.php
+++ src/views/part/changeModel.php
@@ -32,7 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'data' => $models,
             'visibleCount' => count($models),
             'formatter' => function ($model) {
-                return Html::tag('span', Html::encode($model->title), ['class' => 'label label-default']);
+                return Html::tag('span', Yii::t('hipanel:stock', Html::encode($model->title)), ['class' => 'label label-default']);
             },
             'delimiter' => ',&nbsp; ',
         ]) ?>

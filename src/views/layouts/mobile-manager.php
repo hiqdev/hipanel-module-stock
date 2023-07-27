12e9025c5722c92955e7bb919a0c57facbcff196
Andrii Klochok <andreyklochok@gmail.com>
2023-07-27 21:57:29 +0300

    HP-1476: frontend, minor

--- src/views/layouts/mobile-manager.php
+++ src/views/layouts/mobile-manager.php
@@ -16,7 +16,7 @@ MobileAppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, height=device-height">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>

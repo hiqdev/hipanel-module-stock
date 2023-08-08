36e4c2d0d9287c301aac54ee72f9f52254f64bb2
gadezist <39760138+gadezist@users.noreply.github.com>
2023-08-08 22:48:00 +0300

    HP-1473 Add field description to update part (#151)

--- src/views/part/copy.php
+++ src/views/part/copy.php
@@ -1,8 +1,22 @@
 <?php
 
+use hipanel\modules\stock\models\Part;
+
+/**
+ * @var array|Part[] $models
+ * @var array $currencyTypes
+ * @var array $moveTypes
+ * @var array $suppliers
+ */
+
 $this->title = Yii::t('hipanel:stock', 'Copy Part');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:stock', 'Parts'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<?= $this->render('_form', compact(['models', 'moveTypes', 'suppliers', 'currencyTypes'])) ?>
+<?= $this->render('_form', [
+    'models' => $models,
+    'moveTypes' => $moveTypes,
+    'suppliers' => $suppliers,
+    'currencyTypes' => $currencyTypes,
+  ]) ?>

38ed39fec161b868e3e2ff16530ceab5206307ed
Andrii Klochok <andreyklochok@gmail.com>
2024-11-02 14:03:09 +0200

    HP-2171: autofille move description with order if move description is empty on part create

--- src/views/part/create.php
+++ src/views/part/create.php
@@ -1,17 +1,40 @@
 <?php
 
 use hipanel\modules\stock\models\Part;
+use yii\web\View;
 
 /**
  * @var Part $model
- * @var array|Part[] $models
+ * @var Part[] $models
+ * @var array $moveTypes
+ * @var array $suppliers
+ * @var array $currencyTypes
  */
 
 $this->title = Yii::t('hipanel:stock', 'Create Part');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:stock', 'Parts'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
+$this->registerJs(/** @lang JavaScript */ <<<JS
+(() => {
+  $(document).on("select2:select", "[id$='order_id']", function (event) {
+    const moveDescriptionField = $(event.target).parents(".item").find("[id$='move_descr']");
+    if (moveDescriptionField.val().trim() === "") {
+        moveDescriptionField.val(event.params.data.text);
+    }
+  });
+})();
+JS
+    ,
+    View::POS_LOAD);
+
 ?>
 
-<?= $this->render('_form', compact(['model', 'models', 'moveTypes', 'suppliers', 'currencyTypes'])) ?>
+<?= $this->render('_form', [
+    'model' => $model,
+    'models' => $models,
+    'moveTypes' => $moveTypes,
+    'suppliers' => $suppliers,
+    'currencyTypes' => $currencyTypes,
+]) ?>
 

d3024ec3b28b7750e969febd0ccaec792dca3644
Andrii Klochok <andreyklochok@gmail.com>
2025-11-03 14:59:19 +0200

    HP-2665: add Playwright e2e tests for stock export functionality

---
+++ tests/playwright/model/StockExport.ts
@@ -0,0 +1,20 @@
+import { Page } from "@playwright/test";
+import Index from "@hipanel-core/page/Index";
+
+export default class StockExport {
+  private page: Page;private index: Index;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.index = new Index(page);
+  }
+
+  public async startWtith(url: string) {
+    await this.page.goto(url);
+
+    await this.page.getByPlaceholder("description").first().fill("test");
+    await this.index.submitSearchButton();
+
+    await this.index.testExport();
+  }
+}

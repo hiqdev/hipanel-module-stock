ee0ef475e0ac88f2a3af2e555b6fd3f350929ef9
Andrii Klochok <andreyklochok@gmail.com>
2025-11-21 14:28:53 +0200

    HP-2804: refactor Playwright tests and helper classes, update imports, improve typing consistency, and enhance overall structure

--- tests/playwright/e2e/manager/part/part-view.scep.ts
+++ tests/playwright/e2e/manager/part/part-view.scep.ts
@@ -1,17 +1,15 @@
-import { test } from "@hipanel-core/fixtures";
-import {expect} from "@playwright/test";
+import { expect, test } from "@hipanel-core/fixtures";
 import PartIndexView from "@hipanel-module-stock/page/PartIndexView";
 
+test("Ensure I can look parts buyer on selling representation after filtering @hipanel-module-stock @manager", async ({ page }) => {
+  const partIndexView = new PartIndexView(page);
 
-test("Ensure I can look parts buyer on selling representation after filtering @hipanel-module-stock @manager", async ({page }) => {
-    const partIndexView = new PartIndexView(page);
+  await partIndexView.navigateSelling();
+  await partIndexView.applyFiltersByBuyer("solex");
 
-    await partIndexView.navigateSelling();
-    await partIndexView.applyFiltersByBuyer("solex");
+  const td = page.locator(
+    "table.table.table-striped.table-bordered.table-condensed > tbody > tr:first-child > td:nth-child(2)",
+  );
 
-    const td =  page.locator(
-        'table.table.table-striped.table-bordered.table-condensed > tbody > tr:first-child > td:nth-child(2)'
-    )
-
-    await expect(td).toContainText("solex");
+  await expect(td).toContainText("solex");
 });

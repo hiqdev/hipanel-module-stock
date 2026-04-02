f3dc2e58d0cca0bdeba7d1cfbd0d91b565ef9ded
Vadym Hrechukha <vadimgrechuha@gmail.com>
2026-04-02 11:30:26 +0300

    HQD-82: fix timeout problem with export in playwright tests

--- tests/playwright/e2e/manager/model/export.spec.ts
+++ tests/playwright/e2e/manager/model/export.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@hipanel-core/fixtures";
 import StockExport from "@hipanel-module-stock/model/StockExport";
 
 test("model export works correctly @hipanel-module-stock @manager", async ({ page }) => {
+  test.setTimeout(180_000);
   const exportPage = new StockExport(page);
 
   await exportPage.startWith("/stock/model/index");
--- tests/playwright/e2e/manager/part/export.spec.ts
+++ tests/playwright/e2e/manager/part/export.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@hipanel-core/fixtures";
 import StockExport from "@hipanel-module-stock/model/StockExport";
 
 test("part export works correctly @hipanel-module-stock @manager", async ({ page }) => {
+  test.setTimeout(180_000);
   const exportPage = new StockExport(page);
 
   await exportPage.startWith("/stock/part/index");
